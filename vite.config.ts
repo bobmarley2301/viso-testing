bb7ca4ab6e265324bd884c05adaec2d528a5ba25
Dmitro Yaremchuk <145034908+bobmarley2301@users.noreply.github.com>
2025-03-06 11:51:00 +0200

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,67 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
+import viteCompression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	build: {
-		outDir: 'dist',
-		sourcemap: true,
-		minify: 'terser',
-		terserOptions: {
-			compress: {
-				drop_console: true,
-				drop_debugger: true,
-			},
-		},
-		rollupOptions: {
-			output: {
-				manualChunks: {
-					vendor: [
-						'react',
-						'react-dom',
-						'@chakra-ui/react',
-						'@tanstack/react-router',
-						'@tanstack/react-query',
-						'axios',
-						'zustand',
-					],
-					ui: [
-						'@chakra-ui/icons',
-						'@emotion/react',
-						'@emotion/styled',
-						'framer-motion',
-					],
-				},
-			},
-		},
-		chunkSizeWarningLimit: 1000,
-		assetsInlineLimit: 4096,
-	},
-	server: {
-		port: 3000,
-		open: true,
-	},
-})
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: "gzip",
+      ext: ".gz",
+    }),
+    viteCompression({
+      algorithm: "brotliCompress",
+      ext: ".br",
+    }),
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+      filename: "dist/stats.html",
+    }),
+  ],
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: [
+            "react",
+            "react-dom",
+            "@chakra-ui/react",
+            "@tanstack/react-router",
+            "@tanstack/react-query",
+            "axios",
+            "zustand",
+          ],
+          ui: [
+            "@chakra-ui/icons",
+            "@emotion/react",
+            "@emotion/styled",
+            "framer-motion",
+          ],
+        },
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+        assetFileNames: "assets/[ext]/[name]-[hash].[ext]",
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+    assetsInlineLimit: 4096,
+    reportCompressedSize: false,
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
